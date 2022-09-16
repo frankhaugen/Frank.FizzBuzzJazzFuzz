4f60eda607b69a5c77828ad7d403d5789bd147d4
Frank R. Haugen <frank.haugen@gmail.com>
2022-09-16 18:28:41 +0200

    Some metadata "fun"

--- Frank.FizzBuzzJazzFuzz.Cli/Program.cs
+++ Frank.FizzBuzzJazzFuzz.Cli/Program.cs
@@ -1,6 +1,18 @@
-﻿using Frank.FizzBuzzJazzFuzz.FizzBuzz;
+﻿using System.Diagnostics;
 
+using Frank.FizzBuzzJazzFuzz.FizzBuzz;
+
+var stopwatch = Stopwatch.StartNew();
 var fizzbuzzRunner = new FizzBuzzRunner();
-var result = fizzbuzzRunner.Run(100);
+var result = fizzbuzzRunner.Run(100_000_000);
 
-Console.WriteLine(string.Join("\n", result));
+Console.WriteLine($"{result.Count.ToString("N")} Iterations, ran for {stopwatch.Elapsed.ToString("g")} seconds and uses {(GC.GetGCMemoryInfo().TotalCommittedBytes / 1024 / 1024).ToString("##,### MB")} RAM");
+stopwatch.Stop();
+Console.ReadLine();
+stopwatch.Start();
+result.ForEach(Console.WriteLine);
+
+Console.WriteLine($"Total time spent: {stopwatch.Elapsed:g}");
+Console.WriteLine($"Hit RETURN to close...");
+
+Console.ReadLine();
